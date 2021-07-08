b6e37d67d1b61734b8e8156422bf7f2da4212a96
Clayton-Roberts <cnr31@cam.ac.uk>
2021-07-08 11:04:49 +0100

    Added summarise_results file
    This file will contain a class definition to load model results into, can print results as tables, stuff like that.

--- src/main.py
+++ src/main.py
@@ -6,11 +6,11 @@ from src import fit_model as fm
 #-------------------------------------------------------
 #    --- Testing suite ---
 #-------------------------------------------------------
-tsf.generate_mu_and_Sigma()
-tsf.generate_alphas_betas_and_gammas(10)
-tsf.generate_dataset()
-tsf.prepare_dataset_for_cmdstanpy()
+# tsf.generate_mu_and_Sigma()
+# tsf.generate_alphas_betas_and_gammas(10)
+# tsf.generate_dataset()
+# tsf.prepare_dataset_for_cmdstanpy()
 # This next function doesn't have to be run each time, but it won't re-download if CmdStan is already in the
 # indicated directory, so it's not the end of the world if you re-run it by accident.
-#fm.install_cmdstan()
-fm.fit_model('test_suite/data.json', 'models/linear_hierarchical_model.stan', 'test_run')
+# fm.install_cmdstan()
+# fm.fit_model('test_suite/data.json', 'models/linear_hierarchical_model.stan', 'test_run')
