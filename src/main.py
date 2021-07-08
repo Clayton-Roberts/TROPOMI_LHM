953dd69ff8320f9c2698823887f7c39ea3fad51b
Clayton-Roberts <cnr31@cam.ac.uk>
2021-07-08 10:23:09 +0100

    Testing to see if outputs are not committed correctly.

--- src/main.py
+++ src/main.py
@@ -12,5 +12,5 @@ tsf.generate_dataset()
 tsf.prepare_dataset_for_cmdstanpy()
 # This next function doesn't have to be run each time, but it won't re-download if CmdStan is already in the
 # indicated directory, so it's not the end of the world if you re-run it by accident.
-fm.install_cmdstan()
+#fm.install_cmdstan()
 fm.fit_model('test_suite/data.json', 'models/linear_hierarchical_model.stan', 'test_run')
