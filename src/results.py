cff7c8795653aa88375bd95d0ae86147f699e252
Clayton-Roberts <cnr31@cam.ac.uk>
2021-07-11 08:53:04 +0100

    Successfully writes reduced chi-squared csv.
    FittedModel class can successful write the reduced chi-squared csv to the relevant dropout folder.

--- src/results.py
+++ src/results.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 from tabulate import tabulate
 import random
+import csv
 
 class FittedModel:
     '''This class loads the results of a fitted model and organises the results in a useful way. When initialised it
@@ -14,34 +15,70 @@ class FittedModel:
         and then writes it to a .csv file. This is performed only as part of the dropout testing.
         '''
 
-    
+        # Open the dropout_dataset.csv file
+        dropout_df = pd.read_csv('data/' + self.run_name + '/dropout_dataset.csv')
 
+        with open('data/' + self.run_name + '/reduced_chi_squared.csv', 'w') as csvfile:
+            csv_writer = csv.writer(csvfile, delimiter=',')
+            csv_writer.writerow(('Day_ID', 'Date', 'Reduced_chi_squared', 'N_observations'))
 
+            for day_id in set(dropout_df.Day_ID):
 
-    def predict_ch4(self, obs_no2, sigma_N, date=None, day_id=None):
+                dropout_day_df = dropout_df[dropout_df.Day_ID == day_id]
+
+                date = dropout_day_df.Date.iloc[0]
+
+                obs_CH4 = list(dropout_day_df.obs_CH4)
+                sigma_C = list(dropout_day_df.sigma_C)
+                obs_NO2 = list(dropout_day_df.obs_NO2)
+                sigma_N = list(dropout_day_df.sigma_N)
+
+                n_Observations  = len(obs_CH4)
+                degrees_freedom = int(n_Observations - 3)
+
+                pred_CH4       = []
+                sigma_pred_CH4 = []
+
+                for i in range(len(obs_NO2)):
+                    prediction, uncertainty = self.predict_ch4(obs_NO2[i], sigma_N[i], 'day_id', day_id)
+                    pred_CH4.append(prediction)
+                    sigma_pred_CH4.append(uncertainty)
+
+                chi_squared = np.sum(
+                    [(o_i - p_i) ** 2 / (sigma_o_i ** 2 + sigma_p_i ** 2) for o_i, p_i, sigma_o_i, sigma_p_i in
+                     zip(obs_CH4, pred_CH4, sigma_C, sigma_pred_CH4)])
+
+                reduced_chi_squared = chi_squared / degrees_freedom
+
+                csv_writer.writerow([day_id, date, reduced_chi_squared, n_Observations])
+
+    def predict_ch4(self, obs_no2, sigma_N, idientifier, value):
         '''
         This function is for predicting an observed value of CH4 with an associated standard deviation on the estimate.
 
-        :param date: Optional, date that we want to make the prediction on, must be in the format YYYYMMDD.
-        :type date: int
         :param obs_no2: The observed value of NO2 in micro mol / m^2
         :type obs_no2: float
         :param sigma_N: The reported error on the observation of NO2, also in micro mol / m^2.
         :type sigma_N: float
-        :param day_id: Optional, Day ID that can be optionally provided.
-        :type day_id: int
+        :param identifier: Either 'date' or 'day_id'.
+        :type idientifier: str
+        :param value: Value of either 'date' or 'day_id'. If year, then value must be in format YYYYMMDD.
+        :type value: int
         :return: A value for predicted CH4 and associated uncertainty.
         '''
 
-
         # Open the full dataset.csv to see which day id we need.
-        if date:
+
+        if idientifier == 'date':
             if 'dropout' in self.run_name:
                 dataset_df = pd.read_csv('data/' + self.run_name.split('/')[0] + '/dataset.csv')
             else:
                 dataset_df = pd.read_csv('data/' + self.run_name + '/dataset.csv')
 
-            day_id     = dataset_df[dataset_df.Date == date].Day_ID.iloc[0]
+            day_id     = dataset_df[dataset_df.Date == value].Day_ID.iloc[0]
+
+        elif idientifier == 'day_id':
+            day_id = value
 
         alphas = self.full_trace['alpha.' + str(day_id)]
         betas  = self.full_trace['beta.' + str(day_id)]
