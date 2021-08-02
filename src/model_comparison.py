1a9eb96b3adbb7751268d64b7d064d7466479271
Clayton-Roberts <cnr31@cam.ac.uk>
2021-08-02 15:55:43 +0100

    Pushing to merge
    Have create two files called test_analysis.py and real_analysis.py . Everything from data generation to model comparison and plotting is run from these files, essentially we are segregating things to do with test data from things to do with real data. Have also deleted main.py as it was getting cluttered. May need further file reorganisation later but that's not a big deal.

--- src/model_comparison.py
+++ src/model_comparison.py
@@ -1,17 +1,18 @@
+from src import constants as ct
 import numpy as np
 
-def compare_models(individual_model, mean_error_model):
+def compare_models(individual_error_model, mean_error_model):
     '''A function to calculate expected log pointwise predictive density for each of the two models and compare
     the two.
 
-    :param individual_model: A loaded FittedModel for when a model was fitted using per-observation uncertainty.
-    :type individual_model: FittedModel
+    :param individual_error_model: A loaded FittedModel for when a model was fitted using per-observation uncertainty.
+    :type individual_error_model: FittedModel
     :param mean_error_model: A loaded FittedModel for when a model was fitted using mean daily observational error.
     :type individual_model: FittedModel
     '''
 
     print("Calculating for individual error per observation model...")
-    individual_model_elpd_i = individual_model.elpd_i()
+    individual_model_elpd_i = individual_error_model.elpd_i()
     individual_model_elpd   = sum(individual_model_elpd_i)
     print("Individual error model estimated elpd: ", round(individual_model_elpd, 2))
 
@@ -32,5 +33,19 @@ def compare_models(individual_model, mean_error_model):
 
     print('Difference in elpd: {:.2f}'.format(diff))
     print('Standard error of difference: {:.2f}'.format(se_diff))
+
+    f = open(ct.FILE_PREFIX + "/outputs/" + mean_error_model.run_name + "/model_comparison.txt", "a")
+    f.write("Daily mean error model estimated elpd: " + str(round(mean_error_model_elpd, 2)) + '\n')
+    f.write("Individual error model estimated elpd: " + str(round(individual_model_elpd, 2)) + '\n')
+    f.write('Difference in elpd: {:.2f}'.format(diff) + '\n')
+    f.write('Standard error of difference: {:.2f}'.format(se_diff) + '\n')
+    f.close()
+
+    g = open(ct.FILE_PREFIX + "/outputs/" + individual_error_model.run_name + "/model_comparison.txt", "a")
+    g.write("Daily mean error model estimated elpd: " + str(round(mean_error_model_elpd, 2)) + '\n')
+    g.write("Individual error model estimated elpd: " + str(round(individual_model_elpd, 2)) + '\n')
+    g.write('Difference in elpd: {:.2f}'.format(diff) + '\n')
+    g.write('Standard error of difference: {:.2f}'.format(se_diff) + '\n')
+    g.close()
 
 
